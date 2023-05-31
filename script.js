ef44d2e830997c5c9bc96b22bb9a3b16de322447
Aradhya <132876720+Aradhyapw@users.noreply.github.com>
2023-06-01 01:05:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var innerDiv = document.getElementById('inner');
+innerDiv.addEventListener('click',function(){
+	console.log('Click Inner Div');
+	});
+
+// var outerDiv = document.getElementById('outer');
+// outerDiv.addEventListener('click',function(){
+// 	console.log('Click outer Div');
+// 	});
+
+// document.addEventListener('click',function(){
+// 	console.log('Click document');
+// });
+		
+		
